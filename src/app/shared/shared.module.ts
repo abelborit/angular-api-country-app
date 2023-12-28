70f39028231ee6fef8db1293e06bd475ac90a206
Abel Borit <abelboritguitton@gmail.com>
2023-12-28 15:42:58 -0500

    [FEAT] create sidebar.component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from '../app-routing.module';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent],
-  exports: [HomePageComponent, AboutPageComponent],
-  imports: [CommonModule],
+  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent],
+  exports: [HomePageComponent, AboutPageComponent, SidebarComponent],
+  imports: [CommonModule, AppRoutingModule],
 })
 export class SharedModule {}
