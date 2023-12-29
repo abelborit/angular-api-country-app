f276a1722ac02f073e0afdb763d6b433bdc12570
Abel Borit <abelboritguitton@gmail.com>
2023-12-29 12:04:40 -0500

    [FIX] change imports and add RouterModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
@@ -21,6 +20,6 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
     SidebarComponent,
     ContactPageComponent,
   ],
-  imports: [CommonModule, AppRoutingModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
