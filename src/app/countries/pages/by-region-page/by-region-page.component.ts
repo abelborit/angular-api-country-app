564f3542614223cf28421f3d4e1067bd584221c8
Abel Borit <abelboritguitton@gmail.com>
2024-01-02 21:54:21 -0500

    [FEAT] by-region-page.component select region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { CountryInterface } from '../../interfaces/country.interface';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-countries-pages-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -13,10 +15,19 @@ export class ByRegionPageComponent {
 
   public regionSearchedValue: string = '';
   public countriesData: CountryInterface[] = [];
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
 
-  handleSearchByRegion(searchValue: string): void {
+  handleSearchByRegion(searchValue: Region): void {
     // console.log('ByRegionPageComponent - handleSearchByRegion');
     // console.log({ searchValue });
+    this.selectedRegion = searchValue;
     this.regionSearchedValue = searchValue;
 
     this.countriesService
